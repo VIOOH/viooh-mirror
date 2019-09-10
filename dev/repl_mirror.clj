75a52d367f4bd9147d25eeb87063c067077f75e0
Bruno Bonacci <bruno.bonacci@gmail.com>
2019-09-10 16:24:34 +0100

    update repl session info

--- dev/repl_mirror.clj
+++ dev/repl_mirror.clj
@@ -14,34 +14,29 @@
 
   (in-ns 'viooh.mirror.mirror)
 
-  (def group-id-prefix "test")
-  (def serdes [:string :avro])
   (def mirror-cfg
-    {:name "prv_DigitalReservation_PT"
-     ;;TODO CHANGE CFG
-     :mirror-mode :lenient
-     ;;TODO CHANGE CFG
-     :subject-naming-strategy :topic-name
-     :source
-     {:kafka {:bootstrap.servers "kf1.dataplatform.jcdecaux.com:9092,kf2.dataplatform.jcdecaux.com:9092,kf3.dataplatform.jcdecaux.com:9092"
-              :max.partition.fetch.bytes "1000000"
-              :max.poll.records "50000"
-              :auto.offset.reset "earliest"}
-      :topic {:topic-name "prv_DigitalReservation_PT"}
-      :schema-registry-url "http://registry.dataplatform.jcdecaux.com"}
+    (#'viooh.mirror.main/apply-single-mirror-default
+     {}
+     {:mirror-mode :lenient
+      :source
+      {:kafka {:bootstrap.servers "kf1.dataplatform.jcdecaux.com:9092,kf2.dataplatform.jcdecaux.com:9092,kf3.dataplatform.jcdecaux.com:9092"
+               :max.partition.fetch.bytes "1000000"
+               :max.poll.records "50000"
+               :auto.offset.reset "earliest"}
+       :topic {:topic-name "prv_DigitalReservation_PT"}
+       :schema-registry-url "http://registry.dataplatform.jcdecaux.com"}
 
-     :destination
-     {:kafka {:bootstrap.servers "10.1.151.67:9092,10.1.152.252:9092,10.1.153.241:9092"}
-      :topic {:topic-name "prd.datariver.prv_DigitalReservation"}
-      :schema-registry-url "https://schema-registry.dev.develop.farm"}
+      :destination
+      {:kafka {:bootstrap.servers "10.1.151.67:9092,10.1.152.252:9092,10.1.153.241:9092"}
+       :topic {:topic-name "prd.datariver.prv_DigitalReservation"}
+       :schema-registry-url "https://schema-registry.dev.develop.farm"}
 
-     :serdes [:string :avro]})
+      :serdes [:string :avro]}))
 
 
   (def closed? (atom false))
   (def p (promise))
-  (def group-id (str/join "_" [group-id-prefix "test"]))
-  (def mirror-name group-id)
+  (def mirror-name (:name mirror-cfg))
   (def source (:source mirror-cfg))
   (def destination (:destination mirror-cfg))
   (def src-schema-registry-url (:schema-registry-url source))
@@ -50,13 +45,14 @@
   (def src-topic (:topic-name src-topic-cfg))
   (def dest-topic-cfg (:topic destination))
   (def dest-topic (:topic-name dest-topic-cfg))
-  (def src-serdes (s/serdes serdes src-schema-registry-url))
-  (def dest-serdes (s/serdes serdes dest-schema-registry-url))
+  (def src-serdes (s/serdes (:serdes mirror-cfg) src-schema-registry-url))
+  (def dest-serdes (s/serdes (:serdes mirror-cfg) dest-schema-registry-url))
+  (def consumer-group-id (:consumer-group-id mirror-cfg))
 
   (sm/mirror-schemas mirror-cfg)
 
   ;; start consumer and producers
-  (def c (consumer group-id source src-serdes))
+  (def c (consumer consumer-group-id source src-serdes))
   (def p (producer destination dest-serdes))
   (def k-subs (k/subscribe c [src-topic-cfg]))
 
