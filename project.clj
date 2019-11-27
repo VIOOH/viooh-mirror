dba8cffb82e3a8c4b4aa0b4a830c1e4babccb4a8
Olivier Bohrer <olivier.bohrer@gmail.com>
2019-11-27 19:05:03 +0000

    bump kafka-ssl-helper

--- project.clj
+++ project.clj
@@ -26,7 +26,7 @@
                  [io.confluent/kafka-schema-registry-client "5.1.2"
                   :exclusions [com.fasterxml.jackson.core/jackson-databind]]
 
-                 [viooh/kafka-ssl-helper "0.3.0"]
+                 [viooh/kafka-ssl-helper "0.4.1"]
 
                  ;;logging madness
                  [org.clojure/tools.logging "0.5.0"]
