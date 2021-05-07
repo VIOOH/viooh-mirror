3b53073abb32816652e6c5e895cd06cb11c46600
Bruno Bonacci <bruno.bonacci@gmail.com>
2021-05-07 13:08:57 +0100

    Dependencies update (#9)
    * updating some of the dependencies
    
    * adding kafka and jvm samplers for monitoring

--- project.clj
+++ project.clj
@@ -8,12 +8,12 @@
 
   :repositories [["confluent" {:url "https://packages.confluent.io/maven/"}]]
 
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
 
                  [samsara/trackit-core "0.9.3"]
                  [samsara/trackit-prometheus "0.9.3"]
                  [integrant "0.8.0"]
-                 [http-kit "2.3.0"]
+                 [http-kit "2.5.3"]
                  [cheshire "5.10.0"]
                  [metosin/compojure-api "1.1.13"]
 
@@ -24,20 +24,23 @@
 
                  [com.viooh/kafka-ssl-helper "0.9.0"]
 
-                 [fundingcircle/jackdaw "0.7.1"]
+                 [fundingcircle/jackdaw "0.7.10"]
                  [io.confluent/kafka-schema-registry-client "5.4.1"
                   :exclusions [com.fasterxml.jackson.core/jackson-databind]]
 
                  ;;logging
-                 [org.clojure/tools.logging "1.0.0"]
+                 [org.clojure/tools.logging "1.1.0"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [org.codehaus.janino/janino "3.1.1"] ;; logback configuration conditionals :(
+                 [org.codehaus.janino/janino "3.1.3"] ;; logback configuration conditionals :(
                  [com.internetitem/logback-elasticsearch-appender "1.6"]
 
                  ;; observability
-                 [com.brunobonacci/mulog "0.2.0"]
-                 [com.brunobonacci/mulog-elasticsearch "0.2.0"]
-                 [com.brunobonacci/mulog-kafka "0.2.0"]]
+                 [com.brunobonacci/mulog               "0.7.1"]
+                 [com.brunobonacci/mulog-elasticsearch "0.7.1"]
+                 [com.brunobonacci/mulog-kafka         "0.7.1"]
+                 [com.brunobonacci/mulog-cloudwatch    "0.7.1"]
+                 [com.brunobonacci/mulog-mbean-sampler "0.7.1"]
+                 [com.brunobonacci/mulog-jvm-metrics   "0.7.1"]]
 
   :main viooh.mirror.main
 
@@ -45,7 +48,7 @@
 
   :profiles {:uberjar {:aot :all}
              :dev {:jvm-opts ["-D1config.default.backend=fs"]
-                   :dependencies [[midje "1.9.9"]
-                                  [org.clojure/test.check "1.0.0"]
-                                  [criterium "0.4.5"]]
+                   :dependencies [[midje "1.9.10"]
+                                  [org.clojure/test.check "1.1.0"]
+                                  [criterium "0.4.6"]]
                    :plugins      [[lein-midje "3.2.2"]]}})
